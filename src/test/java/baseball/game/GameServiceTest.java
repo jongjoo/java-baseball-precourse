a61800720e41230cbd63854f8b99ea96dea1f368
jongjoo <jjongju8790@gmail.com>
2022-04-17 11:47:41 +0900

    feat: 사용자에게 입력받은 값을 검증하는 기능

--- src/test/java/baseball/game/GameServiceTest.java
+++ src/test/java/baseball/game/GameServiceTest.java
@@ -1,19 +1,19 @@
 package baseball.game;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
-import java.util.Stack;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameServiceTest {
 
@@ -56,7 +56,7 @@ class GameServiceTest {
         assertEquals(SIZE_NUMBER, set.size(), "서로 다른 값이 존재하지 않습니다.");
     }
 
-    public void selectRandomNumber(){
+    public void selectRandomNumber() {
         while (!isLimitSize()) {
             int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER);
             addNumber(randomNumber);
@@ -78,14 +78,36 @@ class GameServiceTest {
     }
 
 
-    @DisplayName("")
-    @Test
-    void scannerClientNumber() {
+    @DisplayName("사용자에게 올바른 3개의 숫자를 받았는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789", "126", "513"})
+    void scannerClientNumber(String inputs) {
 
+        // given
+        // when
+        List<Integer> inputList = new ArrayList<>();
+        for (int i = 0; i < inputs.length(); i++){
+            inputList.add((int) inputs.charAt(i));
+        }
+        isDuplicateNumber(inputList);
 
+        // then
+        assertEquals(SIZE_NUMBER, inputList.size(), "서로 다른 값이 존재하지 않습니다.");
     }
 
-    @Test
-    void compareGameNumber() {
+    public boolean isDuplicateNumber(List<Integer> inputList){
+        Set<Integer> set = new HashSet<>(inputList);
+        if(set.size() == SIZE_NUMBER){
+            return true;
+        }
+        return false;
+    }
+
+    @DisplayName("두값을 비교한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"123:567", "123:153", "123:172", "123:325", "123:123"}, delimiter = ':')
+    void compareGameNumber(String randomNumber, String inputNumber) {
+
+        
     }
 }
